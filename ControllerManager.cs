01b4233d1b6c0c9374fc9ed7d7e696e4d87727ad
Blaise Thomas <blaisegthomas@gmail.com>
2016-11-23 15:49:25 -0800

    controller manager script

---
+++ ControllerManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerManager : MonoBehaviour {
+
+    public GameObject controllerPivot;
+    private GameObject player;
+
+    void Awake()
+    {
+        //player = GameObject.Find("Player");
+        //controllerPivot.transform.localRotation = player.transform.rotation;
+    }
+
+    void Update()
+    {
+        UpdatePointer();
+    }
+
+    private void UpdatePointer()
+    {
+        
+        controllerPivot.SetActive(true);
+        controllerPivot.transform.rotation = GvrController.Orientation;
+        
+    }
+}
